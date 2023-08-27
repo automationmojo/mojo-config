25148f7ad574bd0030b49450b04a00eabbd73df8
Myron Walker <myron.walker@gmail.com>
2023-08-27 17:26:22 -0500

    We need a seperate config loader for files

--- source/packages/mojo/config/configurationmaps.py
+++ source/packages/mojo/config/configurationmaps.py
@@ -114,6 +114,7 @@ def resolve_credentials_configuration(ctx: Context, keyphrase: Optional[str] = N
 
     if MOJO_CONFIG_VARIABLES.MJR_CONFIG_CREDENTIAL_FILES is not None:
         for cfile in MOJO_CONFIG_VARIABLES.MJR_CONFIG_CREDENTIAL_FILES:
+            config_loader = ConfigurationLoader([], credentials=credentials)
             config_info = config_loader.load_configuration_from_file(cfile, keyphrase=keyphrase)
             CONFIGURATION_MAPS.CREDENTIAL_CONFIGURATION_MAP.maps.insert(0, config_info)
 
@@ -149,6 +150,7 @@ def resolve_landscape_configuration(ctx: Context, keyphrase: Optional[str] = Non
 
     if MOJO_CONFIG_VARIABLES.MJR_CONFIG_LANDSCAPE_FILES is not None:
         for cfile in MOJO_CONFIG_VARIABLES.MJR_CONFIG_LANDSCAPE_FILES:
+            config_loader = ConfigurationLoader([], credentials=credentials)
             config_info = config_loader.load_configuration_from_file(cfile, keyphrase=keyphrase)
             CONFIGURATION_MAPS.LANDSCAPE_CONFIGURATION_MAP.maps.insert(0, config_info)
 
@@ -182,6 +184,7 @@ def resolve_runtime_configuration(ctx: Context, keyphrase: Optional[str] = None,
 
     if MOJO_CONFIG_VARIABLES.MJR_CONFIG_RUNTIME_FILES is not None:
         for cfile in MOJO_CONFIG_VARIABLES.MJR_CONFIG_RUNTIME_FILES:
+            config_loader = ConfigurationLoader([], credentials=credentials)
             config_info = config_loader.load_configuration_from_file(cfile, keyphrase=keyphrase)
             CONFIGURATION_MAPS.RUNTIME_CONFIGURATION_MAP.maps.insert(0, config_info)
 
@@ -215,6 +218,7 @@ def resolve_topology_configuration(ctx: Context, keyphrase: Optional[str] = None
 
     if MOJO_CONFIG_VARIABLES.MJR_CONFIG_TOPOLOGY_FILES is not None:
         for cfile in MOJO_CONFIG_VARIABLES.MJR_CONFIG_TOPOLOGY_FILES:
+            config_loader = ConfigurationLoader([], credentials=credentials)
             config_info = config_loader.load_configuration_from_file(cfile, keyphrase=keyphrase)
             CONFIGURATION_MAPS.TOPOLOGY_CONFIGURATION_MAP.maps.insert(0, config_info)
 
