2929f0a8066990cb70b75d467edd2412c0f20891
Myron Walker <myron@Myrons-Mac-mini.local>
2024-11-01 22:32:35 -0700

    Add a DataProfileManagerSingleton to mojo-config

--- source/packages/mojo/config/wellknown.py
+++ source/packages/mojo/config/wellknown.py
@@ -8,11 +8,16 @@ from threading import RLock
 
 from mojo.collections.wellknown import ContextSingleton
 from mojo.collections.contextpaths import ContextPaths
+from mojo.collections.mergemap import MergeMap
+
 from mojo.credentials.credentialmanager import CredentialManager
+from mojo.dataprofiles.dataprofilemanager import DataProfileManager
+
 from mojo.config.configurationmaps import CONFIGURATION_MAPS
 
 
 CREDENTIAL_MANAGER_SINGLETON = None
+DATAPROFILE_MANAGER_SINGLETON = None
 
 SINGLETON_LOCK = RLock()
 
@@ -45,3 +50,35 @@ def CredentialManagerSingleton() -> CredentialManager:
             SINGLETON_LOCK.release()
     
     return CREDENTIAL_MANAGER_SINGLETON
+
+
+def DataProfileManagerSingleton() -> DataProfileManager:
+    """
+        Instantiates and gets a global instance of the :class:`DataProfileManager` class.  The
+        :class:`DataProfileManager` provides for management of data source connectivity information profiles.
+    """
+    global DATAPROFILE_MANAGER_SINGLETON
+
+    if DATAPROFILE_MANAGER_SINGLETON is None:
+        SINGLETON_LOCK.acquire()
+        try:
+
+            if DATAPROFILE_MANAGER_SINGLETON is None:
+                dataprofiles = {}
+                if CONFIGURATION_MAPS.LANDSCAPE_CONFIGURATION_MAP is not None and \
+                    len(CONFIGURATION_MAPS.LANDSCAPE_CONFIGURATION_MAP) > 0:
+                    lscape: MergeMap = CONFIGURATION_MAPS.LANDSCAPE_CONFIGURATION_MAP
+                    if "dataprofiles" in lscape:
+                        dataprofiles = lscape["dataprofiles"]
+
+                ctx = ContextSingleton()
+                landscape_uris = ctx.lookup(ContextPaths.CONFIG_LANDSCAPE_URIS, [])
+
+                profilemgr = DataProfileManager()
+                profilemgr.load_datasource_profiles(dataprofiles, source_uris=landscape_uris)
+                DATAPROFILE_MANAGER_SINGLETON = profilemgr
+                
+        finally:
+            SINGLETON_LOCK.release()
+    
+    return DATAPROFILE_MANAGER_SINGLETON
